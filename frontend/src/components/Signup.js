5775455132c55f5a3df88a2a97e0b3528f417ed4
mhjony <mahmudul.jhony@gmail.com>
2020-09-19 21:40:36 +0300

    working on input validation

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect, useRef } from "react";
 
 const Signup = () => {
   const [name, setName] = useState("");
@@ -6,47 +6,32 @@ const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [cpassword, setCpassword] = useState("");
-  const [errors, setErrors] = useState("");
+  //const [errors, setErrors] = useState("");
   const verified = 0;
   const token = "thisistoken93i493hele"
   const recieveEmail = 0
 
-  const validate = () => {
-	  let name = this.state.name;
-	  let username = this.state.username;
-	  let email = this.state.email;
-	  let password = this.state.password;
-	  let cpassword = this.state.cpassword;
+  //new lines
+  const firstRender = useRef(true);
+  const [disable, setDisabled] = useState(true)
+  const [error, setError] = useState(null)
 
-	  let isValid = true;
-	  let errors = {};
+  useEffect(() => {
+	  if (firstRender.current){
+		  firstRender.current = false
+		  return
+	  }
+	  setDisabled(formValidation())
+  }, [name]) // eslint-disable-line react-hooks/exhaustive-deps
 
-	  if (!name){
-		isValid = false;
-		errors["name"] = "Please write your full name";
-	  }
-	  if (!username){
-		  isValid = false;
-		  errors["username"] = "Write your username";
-	  }
-	  if (!email){
-		  isValid = false;
-		  errors["email"] = "Write your email";
-	  }
-	  if (typeof email !== "undefined") {
-          
-        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-        if (!pattern.test(email)) {
-          isValid = false;
-          errors["email"] = "Please enter valid email address.";
-        }
-      }
-	  if (!password || !cpassword){
-		  isValid = false;
-		  errors["password"] = "Type your desired password properly";
-	  }
-	  setErrors(errors);
-	  return (isValid);
+  const formValidation = () => {
+	if (name === "") {
+		setError('Name cant be blank!')
+		return true
+	} else {
+		setError(null)
+		return false
+	}
   }
 
   const onSubmitForm = async e => {
@@ -72,18 +57,18 @@ const Signup = () => {
 			<div className="form-group">
 				<input className="form-control mt-3" 
 					type="text"
-					name="fullname"
+					name="name"
 					value = {name}
 					onChange={e => setName(e.target.value)}
 					placeholder="Full Name"
 				/>
-				<div className="text-danger">{errors}</div>
+				{ error && <p>{error}</p> }
 			</div>
                 <input className="form-control mt-3" type="text" name="username" value = { username } onChange={e => setUsername(e.target.value)} placeholder="Username" required />
                 <input className="form-control mt-3" type="text" name="email" value = { email } onChange={e => setEmail(e.target.value)} placeholder="john@gmail.com" required/>
                 <input className="form-control mt-3" type="password" name="passwd" value = { password } onChange={e => setPassword(e.target.value)} placeholder="Type password" required />
                 <input className="form-control mt-3" type="password" name="cpasswd" value = { cpassword } onChange={e => setCpassword(e.target.value)} placeholder="Confirm password" required />
-                <button className="btn btn-success mt-3">Register</button>
+                <button className="btn btn-success mt-3" type="submit" disabled={disable}>Register</button>
             </form>
         </div>
     </Fragment>
