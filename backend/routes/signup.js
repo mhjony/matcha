102cbb640f09e942a48cc3bbf9e29114b191f7ef
mhjony <mahmudul.jhony@gmail.com>
2020-09-21 20:44:47 +0300

    working with date of birth

--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -5,11 +5,11 @@ const router = express.Router();
 
 router.post("/", async(req, res) => {
     try{
-    const { name, username, email, verified, token, password, recieveEmail } = req.body;
+    const { name, username, email, verified, token, password, recieveEmail, gender } = req.body;
     // const { name, username, email } = req.body;
 		
-		const newUser = await pool.query("INSERT INTO users (name, username, email, verified, token, password, recieveEmail) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *", 
-    [name, username, email, verified, token, password, recieveEmail]);
+		const newUser = await pool.query("INSERT INTO users (name, username, email, verified, token, password, recieveEmail, gender) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *", 
+    [name, username, email, verified, token, password, recieveEmail, gender]);
     // const newUser = await pool.query("INSERT INTO users_check (name, email) VALUES($1, $2) RETURNING *", [name, email]);
     
 		res.json(newUser.rows);
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,4 +1,7 @@
 import React, { Fragment, useState, useRef, useEffect } from "react";
+import DatePicker from 'react-datepicker';
+
+import "react-datepicker/dist/react-datepicker.css";
 
 const Signup = () => {
 	const [name, setName] = useState("");
@@ -6,13 +9,20 @@ const Signup = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [cpassword, setCpassword] = useState("");
+	const [gender, setGender] = useState('');
+	const [date, setDate] = useState(new Date());
 	const verified = 0;
-	const token = "newtoken08989today"
 	const recieveEmail = 0
 
 	const firstRender = useRef(true);
 	const [disable, setDisable] = useState(true);
 	const [error, setError] = useState("");
+	const today = new Date();
+
+	// generating token
+	const rand = () =>Math.random(0).toString(36).substr(2);
+	const token_check = (length) =>(rand()+rand()+rand()+rand()).substr(0,length);
+	const token = token_check(40);
 
 	useEffect(() => {
 		if (firstRender.current){
@@ -20,13 +30,33 @@ const Signup = () => {
 			return
 		}
 		setDisable(inputValidation());
-	}, [name]) // eslint-disable-line react-hooks/exhaustive-deps
+	}, [name, username, email, password]) // eslint-disable-line react-hooks/exhaustive-deps
 
 	const inputValidation = () => {
-		if (name === ""){
-			setError("Name field cant be empty")
-			return true
-		}else {
+		if (name === "" || username === "" || email === "" || password === "")
+		{
+			if (name === ""){
+				setError("Name field cant be empty")
+				return true
+			}
+			if (username === ""){
+				setError("Username field cant be empty")
+				return true
+			}
+			if (email === ""){
+				setError("Email field cant be empty")
+				return true
+			}
+			if (password === ""){
+				setError("Password field cant be empty")
+				return true
+			}
+			if (gender === "select"){
+				setError("Select your gender")
+				return true
+			}
+		}
+		else {
 			setError(null);
 			return false;
 		}
@@ -34,16 +64,23 @@ const Signup = () => {
 
 	const submitForm = async e => {
 		e.preventDefault();
-		try {
-			const response = await fetch("http://localhost:5000/signup", {
-				method: "POST",
-				headers: {"content-type": "application/json"},
-				body: JSON.stringify({name, username, email, password, verified, token, recieveEmail})
-			})
-			console.log(response);
-			window.location = "/";
-		}catch(err){
-			console.error(err.message);
+		//console.log(gender);
+		if (!inputValidation())
+		{
+			try {
+				const response = await fetch("http://localhost:5000/signup", {
+					method: "POST",
+					headers: {"content-type": "application/json"},
+					body: JSON.stringify({name, username, email, password, verified, token, recieveEmail, gender})
+				})
+				console.log(response);
+				window.location = "/";
+			}catch(err){
+				console.error(err.message);
+			}
+		}
+		else{
+			alert({error})
 		}
 	}
 	return (
@@ -59,7 +96,6 @@ const Signup = () => {
 							onChange={e => setName(e.target.value)} 
 							placeholder="Full Name" 
 						/>
-						{error}
 					</div>
 					<div className="form-group mt-3">
 						<input className="form-control"
@@ -97,6 +133,24 @@ const Signup = () => {
 							placeholder="Confirm password"
 						/>
 					</div>
+					<div className="form-group mt-3">
+						<select className="form-control btn border" name="gender" value={gender} onChange={e => setGender(e.target.value)}>
+							<option value="select">select</option>
+							<option value="male">Male</option>
+							<option value="female">Female</option>
+							<option value="others">Others</option>
+						</select>
+					</div>
+					<div className="form-group mt-3">
+						<DatePicker className="form-control"
+							selected={date}
+							onChange={date => setDate(date)}
+							name="date"
+							maxDate={today}
+							dateFormat="dd/MM/yyyy"
+						/>
+					</div>
+					
 					<button className="btn btn-success mt-3" disabled={disable} type="submit">Register</button>
 				</form>
 			</div>
