8f7e8953e2b01974af3576e278618974c16619d1
mhjony <mahmudul.jhony@gmail.com>
2020-09-24 12:44:16 +0300

    now my program can find if the email already exists

--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -5,10 +5,15 @@ const router = express.Router();
 const { registerValidation } = require('../validation')
 
 router.post("/", async(req, res) => {
-  // LETS VALIDATE THE DATA BEFORE MAKE A USER
+  // LETS VALIDATE THE DATA BEFORE MAKE A USER and it comes from validation.js file
     const { error } = registerValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-
+  // If the user already exists
+  pool.query("select email from users where email='"+req.body.email+"'", function(err, data){
+    if(data.rows && data.rows.length > 0){
+      return res.status(400).send("This email already exists");
+    }
+  })
     try{
         const { name, username, email, verified, token, password, recieveEmail, gender } = req.body;
         
