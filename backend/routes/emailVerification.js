fbcd26183ff43104e9724369f2492b28e268c5ed
mhjony <mahmudul.jhony@gmail.com>
2020-10-06 23:04:15 +0300

    now user registration 100% done along with email verification

--- backend/routes/emailVerification.js
+++ backend/routes/emailVerification.js
@@ -3,7 +3,7 @@ const pool = require('../db');
 const router = express.Router();
 
 router.get("/", async(req, res) => {
-  verified = 0;
+  verified = 1;
 
   const verifyUser = await pool.query("UPDATE users SET verified = $1 WHERE token = $2", [verified, req.query.token])
   res.redirect('http://localhost:3000');
