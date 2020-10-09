85c7b3dcd5051341aa9189918eed923a51fabfac
mhjony <mahmudul.jhony@gmail.com>
2020-10-09 15:19:42 +0300

    Navbar and react routing has been done for login and signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 //components
+import Nav from './components/Nav'
 import Signup from './components/Signup';
 import Login from './components/Login';
 
 function App() {
   return (
-    <Fragment>
+    <Router>
       <div>
-        <Signup />
-        <Login />
+        <Nav />
+        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
       </div>
-    </Fragment>
+    </Router>
   );
 }
 
